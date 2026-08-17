6518466109d89b7fe805ab2a04e5996f40f44861
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2026-08-17 14:47:34 +0200

    env

--- EmailMicroService/Utilities/EnvironmentVaraibles.cs
+++ EmailMicroService/Utilities/EnvironmentVaraibles.cs
@@ -2,5 +2,12 @@
 {
     public static class EnvironmentVaraibles
     {
+        public static string? SmtpHost => Environment.GetEnvironmentVariable("SMTP_HOST");
+
+        public static int SmtpPort => int.Parse(Environment.GetEnvironmentVariable("SMTP_PORT") ?? "587");
+
+        public static string? SmtpLogin => Environment.GetEnvironmentVariable("SMTP_LOGIN");
+
+        public static string? SmtpKey => Environment.GetEnvironmentVariable("SMTP_KEY");
     }
 }
