5d4507828a9329399944529082aa41a7c24c1e5c
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2026-08-17 15:48:02 +0200

    fix

--- EmailMicroService/Program.cs
+++ EmailMicroService/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
-builder.Services.AddScoped<EmailService>();
+builder.Services.AddSingleton<EmailService>();
 builder.Services.AddHostedService<KafkaConsumerHostedService>();
 
 var app = builder.Build();
